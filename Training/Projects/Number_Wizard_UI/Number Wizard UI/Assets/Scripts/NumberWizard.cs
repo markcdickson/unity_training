2e4e5ad044fe26094dbb7a5e14908885f5299c94
unknown <Mark Dickson>
2018-08-15 21:05:17 +0100

    Final version of Number Wizard UI

---
+++ Training/Projects/Number_Wizard_UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        startGame();
+    }
+
+    void startGame()
+    {
+        nextGuess();
+    }
+
+    public void onPressHigher()
+    {
+        if (min != max)
+        {
+            min = guess + 1;
+            nextGuess();
+        }
+    }
+
+    public void onPressLower()
+    {
+        max = guess-1;
+        nextGuess();
+    }
+
+    void nextGuess()
+    {
+        guess = Random.Range(min, max);
+
+        guessText.text = guess.ToString();
+    }
+}
