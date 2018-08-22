1fb0ac414d5bc6464187aefa2137a9b361049829
unknown <Mark Dickson>
2018-08-22 21:20:50 +0100

    Block Breaker intermediate checkpoint

---
+++ Training/Projects/BlockBreaker/BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    // Configuration Parameters
+    [SerializeField] Paddle paddle;
+    [SerializeField] float xPush = 2.0f;
+    [SerializeField] float yPush = 15.0f;
+
+    // State
+    Vector2 ballPaddleDiff;
+    Boolean hasStarted = false;
+
+    // Use this for initialization
+    void Start () {
+        ballPaddleDiff = transform.position - paddle.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    private void LaunchOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePosition = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        Vector2 ballPosition = paddlePosition + ballPaddleDiff;
+        transform.position = ballPosition;
+    }
+}
---
+++ Training/Projects/BlockBreaker/BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenMaxX = 14.5f;
+    [SerializeField] float screenMinX = 1.0f;
+    [SerializeField] float screenSize = 16.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        float xPosition = (Input.mousePosition.x / Screen.width) * screenSize;
+
+        paddlePosition.x = Mathf.Clamp(xPosition, screenMinX, screenMaxX);
+        transform.position = paddlePosition;
+	}
+}
